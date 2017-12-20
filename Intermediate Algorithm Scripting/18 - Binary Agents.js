bbf7d1132d57b0ae0e78b4d7c1504e6a6701a2a3
Gavin Michael <gavin.michael.ftw@gmail.com>
2017-12-20 20:53:20 +0400

    Add files via upload

---
+++ Intermediate Algorithm Scripting/18 - Binary Agents.js
@@ -0,0 +1,19 @@
+/*
+CHALLENGE :
+Return an English translated sentence of the passed binary string.
+The binary string will be space separated.
+*/
+
+function binaryAgent(str) {
+	var strArr = str.split(' ');
+	var resultsArr = [];
+	strArr.forEach(function (val) {
+		var currChar = parseInt(val, 2);
+		var char = String.fromCharCode(currChar);
+		resultsArr.push(char);
+	});
+
+	return resultsArr.join('');
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
